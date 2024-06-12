ea4493ecd32ff95b8b6384ad54c6ab3f78fd21d3
Ibrahim <imimazim7@gmail.com>
2024-06-12 13:33:22 +0300

    route for corn job

--- server/src/app.ts
+++ server/src/app.ts
@@ -83,6 +83,12 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/match", matchRouter);
 app.use("/api/v1/prediction", predictionRouter);
 app.use("/api/v1/game", gameRouter);
+app.post("/api/v1/cron", (req: Request, res: Response) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Cron Job is running'
+    });
+});
 
 app.options('*', cors(corsOptions));
 
