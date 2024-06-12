9a64b26582971fc48270c55b7518e35f735ca19a
Ibrahim <imimazim7@gmail.com>
2024-06-12 13:18:53 +0300

    added vercel analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,21 @@ import {RouterProvider} from "react-router-dom";
 import AppRouter from "./routes.jsx";
 import AuthProvider from 'react-auth-kit'
 import store from "./features/Auth/store.js";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster } from 'react-hot-toast';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+import {Toaster} from 'react-hot-toast';
+import {Analytics} from "@vercel/analytics/react"
 
 function App() {
     return (
         <>
             <AuthProvider store={store}>
+                <Analytics/>
                 <QueryClientProvider client={new QueryClient()}>
                     <ReactQueryDevtools initialIsOpen={false}/>
                     <RouterProvider router={AppRouter}/>
                 </QueryClientProvider>
-                <Toaster />
+                <Toaster/>
             </AuthProvider>
         </>
     )
